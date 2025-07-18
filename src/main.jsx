9e54ed8b37297c1c08fee5458508e42bf4069706
Rut Vasner <rutivasner@gmail.com>
2025-07-18 11:10:14 +0300

    correct main

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { WordsProvider } from './context/WordsContext.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Trivia from './component/Trivia.jsx'
-import "./style.css"
-import '@fontsource/noto-sans-hebrew';
-import '@fontsource/noto-serif-hebrew';
-import '@fontsource/noto-rashi-hebrew';
-import '@fontsource/ibm-plex-sans-hebrew';
-import '@fontsource/playpen-sans-hebrew';
-import '@fontsource/solitreo';
-createRoot(document.getElementById('root')).render(
-    <WordsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/trivia" element={<Trivia />} />
-        </Routes>
-      </BrowserRouter>
-    </WordsProvider>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode dir="rtl">
+    <App />
+  </React.StrictMode>,
 )
